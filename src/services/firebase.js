ea634dea33850fa4f2f30303e838bee26f208a44
Zach Butler <zach.standley93@gmail.com>
2019-06-05 13:27:13 -0700

    exported firebase database from firebase.js

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -9,5 +9,8 @@ const config = {
 };
 
 firebase.initializeApp(config);
- 
+
 export const auth = firebase.auth();
+export const db = firebase.database();
+
+export const chatRoomsRef = db.ref('Chatrooms');
