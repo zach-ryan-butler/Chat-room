b9ab1082f1034cc90a2e09b7754b64f93a870030
Zach Butler <zach.standley93@gmail.com>
2019-06-09 20:23:37 -0700

    missing semi

--- src/chat/chat.js
+++ src/chat/chat.js
@@ -2,5 +2,5 @@ import '../utils/check-auth.js';
 import ChatApp from './ChatApp.js';
 
 const root = document.getElementById('app');
-const chatApp = new ChatApp()
+const chatApp = new ChatApp();
 root.appendChild(chatApp.render());
