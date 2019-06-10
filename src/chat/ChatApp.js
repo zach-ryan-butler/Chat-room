4bfdb4f6e54479a6bb6b104772acdf76ab470e0f
Zach Butler <zach.standley93@gmail.com>
2019-06-09 20:06:35 -0700

    rendered and appended most components

--- src/chat/ChatApp.js
+++ src/chat/ChatApp.js
@@ -1,6 +1,9 @@
 import Component from '../Component.js';
 import Header from '../shared/Header.js';
-import { chatRoomsRef } from '../services/firebase.js';
+import MessageList from './MessageList.js';
+import { messagesByRoomRef } from '../services/firebase.js';
+import AddMessage from './AddMessage.js';
+import QUERY from '../QUERY.js';
 
 
 class ChatApp extends Component {
@@ -9,15 +12,29 @@ class ChatApp extends Component {
         const dom = this.renderDOM();
         const main = dom.querySelector('main');
 
-        // const query = window.location.search;
-        // const searchParams = new URLSearchParams(query);
-        // const chatId = searchParams.get('key');
+        const query = window.location.search.slice(1);
+        const searchParams = QUERY.parse(query);
+        const id = searchParams.key;
+        
 
         // const name = chatRoomsRef.child(chatId);
         // console.log(name);
 
         const header = new Header();
         dom.insertBefore(header.render(), main);
+
+        const messageList = new MessageList({ messagesData: [] });
+        main.appendChild(messageList.render());
+
+        const addMessage = new AddMessage();
+        main.appendChild(addMessage.render());
+
+        messagesByRoomRef.child(id).on('value', snapshot => {
+            const messagesData = snapshot.val() ? Object.values(snapshot.val()) : [];
+            messageList.update({ messagesData });
+        });
+
+
 
         return dom;
     }
